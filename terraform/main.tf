68f9aa27e76a3000e13798e002608dc894cc8239
skyler <pkphung2001@gmail.com>
2025-09-13 19:59:46 +0700

    minor

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,62 @@ resource "aws_subnet" "private" {
     Name = "MyVPC_PrivateSubnet"
   }
 }
+
+# -----------------------------------------------------------------------------
+# New EC2 Instance and Security Group
+# -----------------------------------------------------------------------------
+
+# Create a security group to allow SSH access
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh_security_group"
+  description = "Allow inbound SSH traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # WARNING: Open to the world, for demonstration purposes only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AllowSSH"
+  }
+}
+
+# Data source to retrieve the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create an EC2 instance in the public subnet
+resource "aws_instance" "public_instance" {
+  # Get the latest Amazon Linux 2 AMI
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
+  tags = {
+    Name = "PublicInstance"
+  }
+}
